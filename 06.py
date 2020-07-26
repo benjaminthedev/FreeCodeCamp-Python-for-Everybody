f3eb2eb5715471beb9c7564f2caba38fbfb8d581
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-07-26 15:42:22 +0100

    Added new answers

---
+++ 06.py
@@ -0,0 +1,8 @@
+# What will print out after running this code:
+
+width = 15
+height = 12.0
+print(height/3)
+
+
+# 4.0
---
+++ 08.py
@@ -0,0 +1,12 @@
+# Given the following code:
+
+temp = "5 degrees"
+cel = 0
+fahr = float(temp)
+cel = (fahr - 32.0) * 5.0 / 9.0
+print(cel)
+
+
+# Which line/lines should be surrounded by try block?
+
+# Lines 3,4
