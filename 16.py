e695347fa5766ac6134a7bdb69c8a411d8da5206
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-07-27 16:52:34 +0100

    New lessons

---
+++ 16.py
@@ -0,0 +1,7 @@
+# What is the value of i in the following code?
+
+word = "bananana"
+i = word.find("na")
+
+
+#Answer = 2
---
+++ 17.py
@@ -0,0 +1,8 @@
+# What is used to indicate a new line in a string?
+
+
+# Answer
+
+# \n
+
+# Just like is JavaScript
