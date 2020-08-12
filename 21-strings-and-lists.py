deca38b82a6d1cecaf449a67bfcba483dd192df2
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-08-12 09:05:34 +0100

    more lessons added

---
+++ 21-strings-and-lists.py
@@ -0,0 +1,11 @@
+# What does n equal in this code?
+
+words = 'His e-mail is q-lar@freecodecamp.org'
+pieces = words.split()
+parts = pieces[3].split('-')
+n = parts[1]
+
+
+# Answer
+
+# lar@freecodecamp.org
---
+++ 23-dictionaries-common-applications.py
@@ -0,0 +1,9 @@
+# What will the following code print?
+
+counts = {'quincy': 1, 'mrugesh': 42, 'beau': 100, '0': 10}
+
+print(counts.get('kris', 0))
+
+# Answer:
+
+0
