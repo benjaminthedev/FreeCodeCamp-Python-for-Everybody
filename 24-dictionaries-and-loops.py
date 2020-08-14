237047efbaec06c29c07472523739b3e9e2a4029
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-08-14 15:59:34 +0100

    More python lessons added

---
+++ 24-dictionaries-and-loops.py
@@ -0,0 +1,11 @@
+# What will the following code print?:
+
+counts = {'chuck': 1, 'annie': 42, 'jan': 100}
+for key in counts:
+    if counts[key] > 10:
+        print(key, counts[key])
+
+# Answer
+
+# annie 42
+# jan 100
---
+++ 25-the-tuples-collection.py
@@ -0,0 +1,15 @@
+# What will the following code print?:
+
+d = dict()
+d['quincy'] = 1
+d['beau'] = 5
+d['kris'] = 9
+for (k, i) in d.items():
+    print(k, i)
+
+    # Answer:
+
+
+'quincy', 1
+'beau', 5
+'kris', 9
